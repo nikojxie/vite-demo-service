517aeb4c2438109745e23ca4767aea718f1bd99a
xiejuunyi <xiejunyi@xingjiesj.com>
2021-12-14 23:03:12 +0800

    init

--- routes/article.js
+++ routes/article.js
@@ -17,4 +17,8 @@ router.get('/:id', function (req,res,next) {
     })
 })
 
+const newArticle = (data) => {
+    const sql = `INSERT INTO blog_article (title, body, pub_time, status, views,article_order, author_id, category_id,type, comment_status, created_time, last_mod_time) VALUES ("test", "test", "2021-12-14", "p", 55, 1, 1, 5, "a", "o", "2021-12-14 22:49:25", "2021-12-14 22:49:25")`;
+}
+
 module.exports = router;
