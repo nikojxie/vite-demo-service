c187af4c31ed45173faea3c9b70246cb4ebbc7a5
xiejunyi <xiejunyi@dobest.com>
2021-12-14 16:44:36 +0800

    init

--- routes/article.js
+++ routes/article.js
@@ -1,24 +1,21 @@
 const express = require('express');   //引入express模块
 const router = express.Router();
-const db = require("../utils/db");     //引入mysql模块
+const {exec} = require("../db/mysql");
+
+const getDetail = (id) => {
+    const sql = `SELECT * FROM blog_article where id = ${id}`;
+    return exec(sql).then(rows => {
+        return rows[0]
+    })
+}
 
 router.get('/:id', function (req,res,next) {
-    const connection = db.start()
+    // res.cookie("token","123123123",{domain: 'localhost', path: '/', maxAge: 24*60*60*1000 ,httpOnly:true})
     const id = req.params.id
-    const sql = `SELECT * FROM blog_article where id = ${id}`;
-    let str = '';
-    connection.query(sql, function (err,result) {
-        if(err){
-            // console.log('[SELECT ERROR]:',err.message);
-            str = '[SELECT ERROR]:' + err.message
-        } else if(!(result && result.length)) {
-            str = '[SELECT ERROR]:' + '未能查询到此id的文章'
-        } else{
-            str = result[0]; //数据库查询结果返回到result中
-        }
-        res.send(str);  //服务器响应请求
-        connection.end();
-    });
+    const result = getDetail(id)
+    return result.then(data => {
+        res.json(data)
+    })
 })
 
 module.exports = router;
--- routes/list.js
+++ routes/list.js
@@ -1,70 +1,68 @@
 const express = require('express');   //引入express模块
 const router = express.Router();
-const db = require('../utils/db')
+const {exec} = require("../db/mysql");
 
 // 获得全部的文章数据
+const getAll = () => {
+    const sql = "SELECT * FROM blog_article where status = 'p' ORDER BY pub_time desc";
+    return exec(sql).then(rows => {
+        return rows
+    })
+}
+
 router.get('/all', function (req,res,next) {
-    const connection = db.start()
-    const sql = 'SELECT * FROM blog_article ORDER BY pub_time desc';
-    let str = '';
-    connection.query(sql, function (err,result) {
-        if(err){
-            // console.log('[SELECT ERROR]:',err.message);
-            str = '[SELECT ERROR]:' + err.message
-        }else{
-            str = result; //数据库查询结果返回到result中
-        }
-        res.send(str);  //服务器响应请求
-        connection.end();
-    });
+    return getAll().then(data => {
+        res.json(data)
+    })
 })
 
 // 获得全部的文章数据归档
+const getAllFile = () => {
+    const sql = `SELECT id,title,pub_time,DATE_FORMAT(pub_time,'%Y-%c') as created_month from blog_article where status = 'p' ORDER BY pub_time desc`;
+    return exec(sql).then(rows => {
+        return rows
+    })
+}
+
 router.get('/file', function (req,res,next) {
-    const connection = db.start()
-    const sql = `SELECT id,title,pub_time,DATE_FORMAT(pub_time,'%Y-%c') as created_month from blog_article ORDER BY pub_time desc`;
-    let str = '';
-    connection.query(sql, function (err,result) {
-        if(err){
-            str = '[SELECT ERROR]:' + err.message
-        }else{
-            if(result && result.length){
-                let map = {};
-                for(let item of result) {
-                    if(map[item.created_month]){
-                        map[item.created_month].push(item)
-                    }else {
-                        map[item.created_month] = [item]
-                    }
-                }
-                str = map
-            }else {
-                str = []
+    return getAllFile().then(data => {
+        let result = ''
+        if(data && data.length){
+            let map = {};
+            for(let item of data) {
+                if(map[item.created_month]) map[item.created_month].push(item)
+                else map[item.created_month] = [item]
             }
+            result = map
+        }else {
+            result = []
         }
-        res.send(str);  //服务器响应请求
-        connection.end();
-    });
+        res.json(result)
+    })
 })
 
 // 分页获取文章列表
-router.get('/page/:pageNum', function (req,res,next) {
-    const connection = db.start()
-    const pageNum = req.params.pageNum
+const getAllByPage = (pageNum) => {
     const startNum = (pageNum - 1) * 10
     const pageSize = 10
-    const dataSql = `SELECT * FROM blog_article ORDER BY pub_time desc limit ${startNum}, ${pageSize}`;
-    const totalRowsSql = `SELECT count(1) FROM blog_article`
-    let str = '';
-    connection.query(dataSql, function (err,list) {
-        connection.query(totalRowsSql, function (err,totalRows) {
-            str = {
-                totalRows: Object.values(totalRows[0])[0], list
-            }
-            res.send(str);  //服务器响应请求
-            connection.end();
+    const sql = `SELECT * FROM blog_article where status = 'p' ORDER BY pub_time desc limit ${startNum}, ${pageSize}`;
+    return exec(sql).then(rows => {
+        return rows
+    })
+}
+const getTotalRows = () => {
+    const sql = `SELECT count(1) FROM blog_article where status = 'p' `
+    return exec(sql).then(totalRows => {
+        return Object.values(totalRows[0])[0]
+    })
+}
+router.get('/page/:pageNum', function (req,res,next) {
+    const pageNum = req.params.pageNum
+    return getAllByPage(pageNum).then(list => {
+        getTotalRows().then(totalRows => {
+            res.json({totalRows,list})
         })
-    });
+    })
 })
 
 module.exports = router;
