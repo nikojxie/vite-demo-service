1235d7f5add1b5b333c5fffd41bb2d5d963a0e36
xiejunyi <xiejunyi@dobest.com>
2021-12-20 14:35:18 +0800

    update

--- controller/auth.js
+++ controller/auth.js
@@ -5,7 +5,7 @@ const login = (username, password) => {
     username = escape(username) // 防止sql注入
     password = genPassword(password)
     password = escape(password)
-    const sql = `SELECT * FROM accounts_bloguser where username = '${username}' and password = '${secretPwd}'`;
+    const sql = `SELECT * FROM accounts_bloguser where username = ${username} and password = ${password}`;
     return exec(sql).then(rows => {
         return rows[0] || {}
     })
