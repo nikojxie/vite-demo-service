872e971f6b829afd371ef8c77b48f5663060db15
xiejunyi <xiejunyi@dobest.com>
2021-12-20 14:23:53 +0800

    update

--- controller/article.js
+++ controller/article.js
@@ -7,6 +7,10 @@ const getDetail = (id) => {
     })
 }
 
+const newArticle = (data) => {
+    const sql = `INSERT INTO blog_article (title, body, pub_time, status, views,article_order, author_id, category_id,type, comment_status, created_time, last_mod_time) VALUES ("test", "test", "2021-12-14", "p", 55, 1, 1, 5, "a", "o", "2021-12-14 22:49:25", "2021-12-14 22:49:25")`;
+}
+
 module.exports = {
     getDetail,
 }
